c4c89a3bbdf832f687a839b1b1ed44ba01cd4fa1
hikigaya007 <bijop007@gmail.com>
2024-01-01 15:27:41 +0530

    backend server created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,11 @@ function App() {
   
 
   return (
-    <>
+    
     <div className="test-mid">
-      <div className='text-sky-400'>hello world testing tailwind</div>
-      <h1 className='test-head'>Bootstrap test</h1>
-      <button type="button" className="btn btn-primary">Primary</button>
+      hello world
     </div>
-    </>
+    
     
   )
 }
