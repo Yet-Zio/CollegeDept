649844c90859c6ef9ffb51492e494f22999afd93
Mahesh Yet Zio <maheshdeluxe53@gmail.com>
2024-01-01 14:31:04 +0530

    add bs, change tw setting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,13 @@ function App() {
   
 
   return (
-    
-      <div className='text-red-500'>hello world testing tailwind</div>
+    <>
+    <div className="test-mid">
+      <div className='text-sky-400'>hello world testing tailwind</div>
+      <h1 className='test-head'>Bootstrap test</h1>
+      <button type="button" className="btn btn-primary">Primary</button>
+    </div>
+    </>
     
   )
 }
