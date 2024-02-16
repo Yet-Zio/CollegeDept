aae110bc4173ff67e273c0c9d55f03c7e4ed5a95
hikigaya007 <bijop007@gmail.com>
2024-02-16 16:37:44 +0530

    created mongoose model and created signup route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,35 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js'
+
+dotenv.config();
+
+
+const PORT = process.env.PORT || 3000 ;
+
+mongoose.connect(process.env.DB_URI)
+    .then(() => {
+        console.log("DB connected");
+    })
+    .catch((err) => {
+        console.log("error caught", err);
+    });
+
+
+
 
 const app = express();
 
-app.listen(3000 , () =>{
-    console.log("server is running at port 3000")
-})
+app.use(express.json());
+
+
+
+app.listen(PORT, () => {
+    console.log("server is running at port 3000");
+});
+
+app.use('/api/auth',authRouter);
+
+
+// creating middleware
---
+++ backend/models/students.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+
+const studentSchema = new mongoose.Schema({
+    firstname:{
+        type: String,
+        default:""
+    },
+    lastname:{
+        type: String,
+        default:""
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    studentID:{
+        type: Number,
+        unique: true,
+        required:true,
+    },
+    password :{
+        type: String,
+        required: true,
+    },
+    dob:{
+        type: Date,
+        required: false,
+    },
+    avatar:{
+        type: String,
+        default:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+    },
+},{timestamps:true});
+
+
+const Student = mongoose.model('Student',studentSchema);
+
+export default Student;
