6a8129d1dc15c66d7ef6b579f8656412db9798f9
Mahesh Yet Zio <maheshdeluxe53@gmail.com>
2024-02-16 19:04:05 +0530

    remove test route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,8 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StudentsLogin from "./components/Auth/StudentsLogin";
 
 function App() {
   return (
-    <Router>
-        <Routes>
-          <Route path="/" element={<div></div>} />
-          <Route path="/slogin" element={<StudentsLogin />} />
-        </Routes>
-    </Router>
+    <div></div>
   );
 }
 
