027ef21fa387ece3e2a634e7cbcb5bf9104c4c79
hikigaya007 <bijop007@gmail.com>
2024-02-25 20:23:59 +0530

    added functionality to admin module

--- frontend/src/components/Admin/pages/AdminBatches.jsx
+++ frontend/src/components/Admin/pages/AdminBatches.jsx
@@ -1,9 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {Trash} from '@phosphor-icons/react'
+import axios from 'axios';
 
 export default function AdminBatches() {
 
-    const options = ['Option 1', 'Option 2', 'Option 3'];
+    const [batch , setBatch] = useState([]);
+
+    useEffect(() =>{
+        axios
+        .get("http://localhost:3000/api/student/getBatch")
+        .then((res) =>{
+          setBatch([...res.data]);
+          console.log(batch);
+          console.log(res.data);
+        })
+        .catch((error) =>{
+          console.log(error)
+        })
+      },[])
+
+        const  handleStudentFetch = (e) =>{
+            setSelectedOption(e);
+            console.log("hhh",selectedOption);
+        };
+
+        
+
     const [selectedOption, setSelectedOption] = useState(null);
 
   return (
@@ -17,33 +39,29 @@ export default function AdminBatches() {
                     <tr>
                         <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">SL.NO</th>
                         <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">BATCH NAME</th>
-                        <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">Year</th>
                         <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900"></th>
                     </tr>
                     </thead>
 
                     <tbody className="divide-y divide-gray-700">
-                    <tr>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">1</td>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Batch 2021</td>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">2021</td>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700 flex"><Trash size={24} className='text-red-500 cursor-pointer me-4'/></td>
-                    </tr>
-                    <tr>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">2</td>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Batch 2022</td>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">2022</td>
-                        <td className="whitespace-nowrap px-4 py-2 text-gray-700 flex"><Trash size={24} className='text-red-500 cursor-pointer me-4'/></td>
-                    </tr>
+                        {batch.map((batchName , index) =>{
+                            return(
+                                <tr key={index}>
+                                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">{index+1}</td>
+                                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">{batchName}</td>
+                                    <td className="whitespace-nowrap px-4 py-2 text-gray-700 flex"><Trash size={24} className='text-red-500 cursor-pointer me-4'/></td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
             </div>
             <div className='flex flex-col'>
                 <span className='text-2xl useinter mt-10 mb-5'>Students</span>
                 <select name='selectedbatch' id='selectedbatch' className='w-1/4 border border-transparent rounded-lg outline-0 focus:border-[#1F2544]'
-                    onChange={(e) => setSelectedOption(e.target.value)}>
+                    onChange={(e)=> handleStudentFetch(e.target.value)}>
                     <option value="">Select a batch...</option>
-                    {options.map((option, index) => (
+                    {batch.map((option, index) => (
                         <option key={index} value={option}>{option}</option>
                     ))}
                 </select>
--- frontend/src/components/Admin/pages/AdminHome.jsx
+++ frontend/src/components/Admin/pages/AdminHome.jsx
@@ -38,7 +38,8 @@ export default function AdminHome(props) {
       axios
       .get("http://localhost:3000/api/student/getBatch")
       .then((res) =>{
-        setBatch(res.data);
+        setBatch([...res.data]);
+        console.log(batch);
         console.log(res.data);
       })
       .catch((error) =>{
@@ -54,37 +55,22 @@ export default function AdminHome(props) {
         <p className='text-2xl useinter ps-5 pt-5 text-black font-bold'>Dashboard</p>
         <p className='text-sm useinter ps-4 pt-1 text-slate-500/75 font-semibold'>Hello Admin, Good {currentGreet()}</p>
         <div className="flex w-full pe-10">
-<<<<<<< HEAD
           <div className='flex flex-col bg-[#1F2544] border border-transparent rounded-lg ms-5 mt-3 w-1/6 h-36 p-4 justify-center items-center cursor-pointer hover:bg-[#1F2544]/75 select-none' onClick={() => {pageFunc("teachers")}}>
             <span className='text-3xl useinter font-bold text-white'>{teacher.length}</span>
-=======
-          <div className='flex flex-col bg-[#1F2544] border border-transparent rounded-lg ms-5 mt-3 w-1/5 md:w-1/6 h-20 md:h-36 p-4 justify-center items-center cursor-pointer hover:bg-[#1F2544]/75 select-none' onClick={() => {pageFunc("teachers")}}>
-            <span className='text-3xl useinter font-bold text-white'>{numoftrs}</span>
->>>>>>> e511fe22f6baf93eab108304196fb02f76213dfa
             <span className='text-sm useinter font-semibold text-white'>Teachers</span>
             <div className='hidden md:flex'>
               <Users size={72} className='text-white'/>
             </div>
           </div>
-<<<<<<< HEAD
-          <div className='flex flex-col bg-[#474F7A] border border-transparent rounded-lg ms-5 mt-3 w-1/6 h-36 p-4 justify-center items-center cursor-pointer hover:bg-[#474F7A]/75 select-none' onClick={() => {pageFunc("batches")}}>
-            <span className='text-3xl useinter font-bold text-white'>{batch.length}</span>
-=======
           <div className='flex flex-col bg-[#474F7A] border border-transparent rounded-lg ms-5 mt-3 w-1/5 md:w-1/6 h-20 md:h-36 p-4 justify-center items-center cursor-pointer hover:bg-[#474F7A]/75 select-none' onClick={() => {pageFunc("batches")}}>
-            <span className='text-3xl useinter font-bold text-white'>{numofbatches}</span>
->>>>>>> e511fe22f6baf93eab108304196fb02f76213dfa
+            <span className='text-3xl useinter font-bold text-white'>{batch.length}</span>
             <span className='text-sm useinter font-semibold text-white'>Batches</span>
             <div className='hidden md:flex'>
               <Student size={72} className='text-white'/>
             </div>
           </div>
-<<<<<<< HEAD
-          <div className='flex flex-col bg-[#81689D] border border-transparent rounded-lg ms-5 mt-3 w-1/6 h-36 p-4 justify-center items-center cursor-pointer hover:bg-[#81689D]/75 select-none' onClick={() => {pageFunc("queries")}}>
-            <span className='text-3xl useinter font-bold text-white'>0</span>
-=======
           <div className='flex flex-col bg-[#81689D] border border-transparent rounded-lg ms-5 mt-3 w-1/5 md:w-1/6 h-20 md:h-36 p-4 justify-center items-center cursor-pointer hover:bg-[#81689D]/75 select-none' onClick={() => {pageFunc("queries")}}>
-            <span className='text-3xl useinter font-bold text-white'>{numofqueries}</span>
->>>>>>> e511fe22f6baf93eab108304196fb02f76213dfa
+            <span className='text-3xl useinter font-bold text-white'>{0}</span>
             <span className='text-sm useinter font-semibold text-white'>Queries</span>
             <div className='hidden md:flex'>
               <PaperPlaneTilt size={64} className='text-white'/>
