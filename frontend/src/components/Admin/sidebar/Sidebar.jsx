5c414bf753c5690c3d4544df1e228a20c1c505e0
Mahesh Yet Zio <maheshdeluxe53@gmail.com>
2024-02-25 18:39:37 +0530

    change cursor to pointer - sidebar

--- frontend/src/components/Admin/sidebar/Sidebar.jsx
+++ frontend/src/components/Admin/sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ export default function Sidebar(props) {
   const pageFunc = props.pageFunc
   const activePage = props.activePage
   return (
-    <div className='flex flex-col min-h-screen w-1/10 md:w-1/10 lg:w-1/6 border-e border-gray-800 bg-[#1F2544] p-5 '>
+    <div className='flex flex-col min-h-screen w-1/10 md:w-1/10 lg:w-1/6 border-e border-gray-800 bg-[#1F2544] p-5'>
         <div className="mb-7 self-center">
             <SideLogo/>
         </div>
