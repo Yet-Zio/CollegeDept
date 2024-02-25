f1dd226897111a03e725d64d7a6ecc9040364f86
Mahesh Yet Zio <maheshdeluxe53@gmail.com>
2024-02-25 17:59:54 +0530

    add queries + fix overflow issues

--- frontend/src/components/Admin/pages/AdminQueries.jsx
+++ frontend/src/components/Admin/pages/AdminQueries.jsx
@@ -5,38 +5,14 @@ import QueryAccordion from './QueryAccordion'
 export default function AdminQueries() {
 
   return (
-    <div className='flex w-full bg-[#cdd4fa] ps-7'>
-      <div className="flex flex-col w-full">
-        <span className='text-2xl useinter underline mt-5 mb-5'>Queries</span>
-        <div className="overflow-x-auto w-1/2">
-          <table className="useinter divide-y-2 divide-gray-700 text-md">
-            <thead className="ltr:text-left rtl:text-right">
-              <tr>
-                <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">SL.NO</th>
-                <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">NAME</th>
-                <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">EMAIL</th>
-                <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900"></th>
-              </tr>
-            </thead>
-
-            <tbody className="divide-y divide-transparent">
-              <tr>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">1</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">ABHIJITH </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">MURALI M</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 flex"><Trash size={24} className='text-red-500 cursor-pointer me-4'/></td>
-              </tr>
-              <QueryAccordion className='self-start'/>
-              <tr>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">2</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">ABHIJITH </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">S RAJ</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 flex"><Trash size={24} className='text-red-500 cursor-pointer me-4'/></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+    <div className='flex flex-col w-screen min-h-screen bg-[#cdd4fa] ps-7 pe-7'>
+      <span className='text-2xl useinter mt-5 mb-2'>Queries</span>
+      <button 
+        className="self-center mt-2 w-1/6 flex pt-3 pb-3 bg-[#474F7A] justify-center items-center rounded-lg font-sans text-white hover:bg-[#474F7A]/75 mb-3">
+        <span className='hidden md:flex'>Clear All</span><Trash className='md:hidden'/>
+      </button>
+      <QueryAccordion number={2} name="Bijo Prasad" email="bijop007@gmail.com" message="I want to play skibidi, nothing else"/>
+      <QueryAccordion number={1} name="Bijo Prasad" email="bijop007@gmail.com" message="Sir, can you come tonight. It's urgent. It's me gay ass bijo"/>
     </div>
   )
 }
