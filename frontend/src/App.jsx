4a1b61937f5cfd38f71c576463ef4c459f62bb0e
hikigaya007 <bijop007@gmail.com>
2024-03-09 18:54:51 +0530

    created contact us component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import StudentsLogin from './components/Auth/StudentsLogin'
 import AdminDashboard from './components/Admin/AdminDashboard'
 import HomePage from "./pages/HomePage";
 import Menu from "./components/homepage/Menu";
+import ContactUs from "./components/contactUs/ContactUs";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/menu" element={<Menu /> }/>
           <Route path="/studentlogin" element={<StudentsLogin/>}/>
           <Route path="/admin" element={<AdminDashboard/>}/>
+          <Route path="/contact-us" element={<ContactUs/>}/>
         </Routes>
     </Router>
   );
