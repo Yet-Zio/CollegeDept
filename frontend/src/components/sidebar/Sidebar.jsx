61fcaf0c4063fe10fd50cdf49cef14d59512c795
hikigaya007 <bijop007@gmail.com>
2024-03-14 11:38:13 +0530

    bug fixed on side bar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -54,8 +54,8 @@ export default function Sidebar(props) {
   }
   return (
     <>
-    <div className='flex flex-col min-h-screen w-1/10 md:w-2/12 lg:w-[260px] border-e border-transparent bg-transparent p-5 me-10 sm:me-16 md:me-10'></div>
-    <div className={`flex flex-col fixed min-h-screen w-1/10 md:w-2/12 lg:w-[260px] border-e border-${borderBg()} bg-[${sidebarBg()}] p-5 ps-2`}>
+    <div className='flex flex-col min-h-screen  w-1/10 md:w-2/12 lg:w-[260px] border-e border-transparent bg-transparent p-5 me-10 sm:me-16 md:me-10'></div>
+    <div className={`flex flex-col fixed min-h-screen items-center w-1/10 md:w-2/12 lg:w-[260px] border-e border-${borderBg()} bg-[${sidebarBg()}] p-5 ps-2`}>
         <div className="mb-7 flex justify-center items-center ps-2">
             <SideLogo/>
         </div>
