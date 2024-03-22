72752e33f324c4ab429a90c86aa4ce053e591556
Dr_Atomic <anandhu2suresh797@gmail.com>
2024-03-22 17:50:39 +0530

    Contact-us Completed

--- frontend/src/components/contactUs/ContactUs.jsx
+++ frontend/src/components/contactUs/ContactUs.jsx
@@ -4,72 +4,76 @@ import contact from '../../assets/contactus.png'
 const handleSubmit = (e) =>{
     e.preventDefault();
 }
-function ContactUs() {
+export default function ContactUs() {
   return (
-    <div className='h-screen w-screen flex justify-center items-center bg-black'>
-        <div className='flex justify-center p-10  rounded-lg border-black bg-white border-4 shadow-xl'>
-            <div className='flex flex-col gap-4 '>
-                <div>
-                    <p className='text-[#dc8733] text-2xl font-bold'>Get In Touch</p>
-                    <span className='text-[#dc8733] text-xs'>We Are Here For You! How Can We Help?</span>
+    <section className="min-h-screen bg-gradient-to-r from-[#bbb7b7] via-[#000000] to-[#c19447] dark:from-[#272727] dark:via-[#4b4b4b] dark:to-[#6d6761] ">
+    <div className="container flex flex-col min-h-screen px-6 py-12 mx-auto">
+        <div className="flex-1 lg:flex lg:items-center lg:-mx-6">
+            <div className="text-white lg:w-1/2 lg:mx-6">
+                <h1 className="text-2xl font-semibold capitalize lg:text-3xl text-[#DC8733]">Get a quote</h1>
+
+                <p className="max-w-xl mt-6">Ask us everything and we would love to hear from you</p>
+
+                <div className="mt-6 space-y-8 md:mt-8">
+                    <p className="flex items-start -mx-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+
+                        <span className="mx-2 text-white truncate w-72">
+                        P.O, Vazhichal, Kudappanamoodu, Kerala 695505
+                        </span>
+                    </p>
+
+                    <p className="flex items-start -mx-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                        </svg>
+
+                        <span className="mx-2 text-white truncate w-72">(0471   )  224 8416</span>
+                    </p>
+
+                    <p className="flex items-start -mx-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                        </svg>
+
+                        <span className="mx-2 text-white truncate w-72">departmentofcs@emc.com</span>
+                    </p>
                 </div>
-                <div className=' w-[300px]'>
-                    <form className='flex gap-2 flex-col '>
-                        <div  className=' flex flex-col gap-1 '>
-                            <p className='text-[#dc8733]  font-semibold'>Enter Your Name :</p>
-                            <input 
-                            type="text"
-                            placeholder='Name'
-                            className=' h-[40px] border-2 p-2 border-[#dc8733] outline-[#dc8733] rounded-lg md: ' />
+
+                
+            </div>
+
+            <div className="mt-8 lg:w-1/2 lg:mx-6">
+                <div className="w-full px-8 py-10 mx-auto overflow-hidden bg-white shadow-2xl rounded-xl dark:bg-[#151515] lg:max-w-xl">
+                    <h1 className="text-xl font-medium text-gray-700 dark:text-gray-200">Contact form</h1>
+
+                    <form className="mt-4">
+                        <div className="flex-1">
+                            <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
+                            <input type="text" placeholder="Devloper" className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-[#151515] dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                         </div>
-                        <div  className='l flex flex-col gap-1'>
-                            <p className='text-[#dc8733]  font-semibold'>Enter Your Email :</p>         
-                            <input 
-                            type="text"
-                            placeholder='Email'
-                            className=' h-[40px] border-2 p-2 border-[#dc8733] outline-[#dc8733] rounded-lg md:'  />
+
+                        <div className="flex-1 mt-6">
+                            <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
+                            <input type="email" placeholder="Devloper@dev.com" className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-[#151515] dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                         </div>
-                        <div className=' flex flex-col gap-1'>
-                            <p className='text-[#dc8733] font-semibold' >Enter Your Message :</p>
-                            <textarea 
-                            className='border-2 p-2 border-[#dc8733] outline-[#dc8733] rounded-lg' 
-                            type="text"
-                            placeholder='Message'
-                            rows={5} />
+
+                        <div className="w-full mt-6">
+                            <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
+                            <textarea className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:bg-[#151515] dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" placeholder="Message"></textarea>
                         </div>
-                        <button
-                        onClick={(e) => handleSubmit(e)} 
-                        className='bg-[#dc8733] text-white font-semibold p-2 rounded-lg hover:bg-[#dc8733]/75'>Contact</button>
+
+                        <button className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#DC8733] rounded-md hover:bg-[#dc8833c0] ">
+                            get in touch
+                        </button>
                     </form>
                 </div>
             </div>
-            <div className='hidden md:flex flex-col justify-around items-center '>
-                    <img src={contact} alt="Lady" className='w-[500px]' />
-                    <div className='flex  flex-col items-start gap-2'>
-                        <div className='flex gap-1'>
-                            <svg xmlns="http://www.w3.org/2000/svg" 
-                            className='size-6'
-                            viewBox="0 0 24 24" fill="#dc8733"><path d="M18.364 17.364L12 23.7279L5.63604 17.364C2.12132 13.8492 2.12132 8.15076 5.63604 4.63604C9.15076 1.12132 14.8492 1.12132 18.364 4.63604C21.8787 8.15076 21.8787 13.8492 18.364 17.364ZM12 15C14.2091 15 16 13.2091 16 11C16 8.79086 14.2091 7 12 7C9.79086 7 8 8.79086 8 11C8 13.2091 9.79086 15 12 15ZM12 13C10.8954 13 10 12.1046 10 11C10 9.89543 10.8954 9 12 9C13.1046 9 14 9.89543 14 11C14 12.1046 13.1046 13 12 13Z"></path></svg>
-                            <p className='select-text'>Vazhichal Kodapannamodu</p>
-                        </div>
-                        <div className='flex gap-1'>
-                            <svg 
-                            className='size-6'
-                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#dc8733"><path d="M21 16.42V19.9561C21 20.4811 20.5941 20.9167 20.0705 20.9537C19.6331 20.9846 19.2763 21 19 21C10.1634 21 3 13.8366 3 5C3 4.72371 3.01545 4.36687 3.04635 3.9295C3.08337 3.40588 3.51894 3 4.04386 3H7.5801C7.83678 3 8.05176 3.19442 8.07753 3.4498C8.10067 3.67907 8.12218 3.86314 8.14207 4.00202C8.34435 5.41472 8.75753 6.75936 9.3487 8.00303C9.44359 8.20265 9.38171 8.44159 9.20185 8.57006L7.04355 10.1118C8.35752 13.1811 10.8189 15.6425 13.8882 16.9565L15.4271 14.8019C15.5572 14.6199 15.799 14.5573 16.001 14.6532C17.2446 15.2439 18.5891 15.6566 20.0016 15.8584C20.1396 15.8782 20.3225 15.8995 20.5502 15.9225C20.8056 15.9483 21 16.1633 21 16.42Z"></path></svg>
-                            <p className='select-text'>0474846446</p>
-                        </div>
-                        <div className='flex gap-1'>
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                            className='size-6'
-                            viewBox="0 0 24 24" fill="#dc8733"><path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM12.0606 11.6829L5.64722 6.2377L4.35278 7.7623L12.0731 14.3171L19.6544 7.75616L18.3456 6.24384L12.0606 11.6829Z"></path></svg>
-                            <p className='select-text cursor-pointer' onClick={() => {window.location.href = 'mailto:emmanuel@gmail.com'}}>emmanuel@gmail.com</p>
-                        </div>
-                    </div>
-
-            </div>
         </div>
     </div>
+</section>
   )
 }
-
-export default ContactUs
