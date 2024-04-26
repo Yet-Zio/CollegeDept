8e7a10a2f0813d3cc9bfb638812389a263a0ebb6
Dr_Atomic <anandhu2suresh797@gmail.com>
2024-04-26 20:53:28 +0530

    Updated Loader

--- frontend/src/components/Common/Loader.jsx
+++ frontend/src/components/Common/Loader.jsx
@@ -1,8 +1,9 @@
 export default function Loader() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen bg-[#1b1b1b]">
-  <div className="rounded-full h-20 w-20 bg-orange-600 animate-ping"></div>
+<div className="relative flex justify-center items-center h-[100dvh] bg-[#131313]">
+    <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-orange-600"></div>
+    <img src="https://www.svgrepo.com/show/509001/avatar-thinking-9.svg"  className="text-orange-400 rounded-full h-28 w-28" />
 </div>
     </>
   );
