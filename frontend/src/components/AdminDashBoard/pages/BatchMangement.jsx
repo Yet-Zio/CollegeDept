57b8bcb9ef29d8e7af39dd3b1c9ccee21524f2f6
hikigaya007 <bijop007@gmail.com>
2024-04-30 21:31:52 +0530

    integrated batch management

--- frontend/src/components/AdminDashBoard/pages/BatchMangement.jsx
+++ frontend/src/components/AdminDashBoard/pages/BatchMangement.jsx
@@ -8,28 +8,39 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { IconButton } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+  
 export default function BatchMangement() {
   return (
-    <>
+    
         <div className="w-[100%] h-[100%] flex flex-col justify-center items-center ">
             <BatchTable/>
             <StudnetTable/>
         </div>
-    </>
+    
   )
 }
 
 
   export function BatchTable() {
-    function createData(slNo, batchName) {
-        return { slNo, batchName };
-      }
-      
-      const rows = [
-        createData(1, 'batch2020'),
-        createData(2, 'batch2069'),
-      ];
+
+    const [batch , setBatch] = useState([]);
+    
+
+    useEffect(() =>{
+      axios
+      .get("http://localhost:3000/api/student/getBatch")
+      .then((res) =>{
+        setBatch([...res.data]);
+        console.log(batch);
+        console.log(res.data);
+      })
+      .catch((error) =>{
+        console.log(error)
+      })
+    },[])
       
     return (
      <div className="w-[90%] h-[40%] flex flex-col justify-center items-center mt-20 me-4 overflow-auto">
@@ -44,12 +55,12 @@ export default function BatchMangement() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {batch.map((row , index) => (
               <TableRow key={row.slNo}>
                 <TableCell align="center" component="th" scope="row">
-                  {row.slNo}
+                  {index + 1}
                 </TableCell>
-                <TableCell align="center">{row.batchName}</TableCell>
+                <TableCell align="center">{row}</TableCell>
                 <TableCell align="center">
                   <IconButton aria-label="delete">
                     <DeleteIcon />
@@ -65,26 +76,39 @@ export default function BatchMangement() {
   }
 export function StudnetTable() {
     const [selectedOption, setSelectedOption] = useState('');
-    function createData(slNo, firstName, lastName, studentId) {
-        return { slNo, firstName, lastName, studentId };
-      }
-      
-      const rows = [
-        createData(1, 'hello', '', '32021990'),
-        createData(2, 'hello', '', '3202199088'),
-        createData(3, 'hello', '', '1005'),
-        createData(4, 'mahesh', '', '10005'),
-      ];
-      const options = [
-        { value: 'option1', label: 'Option 1' },
-        { value: 'option2', label: 'Option 2' },
-        { value: 'option3', label: 'Option 3' },
-        // Add more options as needed
-      ];
+      const [batch , setBatch] = useState([]);
+    
+
+    useEffect(() =>{
+      axios
+      .get("http://localhost:3000/api/student/getBatch")
+      .then((res) =>{
+        setBatch([...res.data]);
+        console.log(batch);
+        console.log(res.data);
+      })
+      .catch((error) =>{
+        console.log(error)
+      })
+    },[])
+
+    const [student , setStudent] = useState([]);
     
       const handleChange = (e) => {
-        console.log(e)
-        setSelectedOption(e.target.value)
+        let batch = e.target.value ; 
+        setSelectedOption(batch)
+        console.log(batch)
+        setStudent([]);
+
+        axios.post("http://localhost:3000/api/student/getStudent" , {batch})
+        .then((res) => {
+            setStudent(res.data);
+            console.log(res)
+            console.log(student)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
       };
   return (
   <>
@@ -97,9 +121,9 @@ export function StudnetTable() {
         onChange={handleChange}
       >
         <option value="">Select an option</option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
+        {batch.map((option , index) => (
+          <option key={index} value={option}>
+            {option}
           </option>
         ))}
       </select>
@@ -117,14 +141,14 @@ export function StudnetTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.slNo}>
+          {student.map((row , index) => (
+            <TableRow key={row._id}>
               <TableCell align="center" component="th" scope="row">
-                {row.slNo}
+                {index + 1 }
               </TableCell>
-              <TableCell align="center">{row.firstName}</TableCell>
-              <TableCell align="center">{row.lastName}</TableCell>
-              <TableCell align="center">{row.studentId}</TableCell>
+              <TableCell align="center">{row.firstname}</TableCell>
+              <TableCell align="center">{row.lastname}</TableCell>
+              <TableCell align="center">{row.studentID}</TableCell>
               <TableCell align="center">
                 <IconButton aria-label="delete">
                   <DeleteIcon />
